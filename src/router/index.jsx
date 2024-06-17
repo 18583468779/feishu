7b7e4aa2bd322db75cfdfe0370c81e303a786102
xiewen <1123654054@qq.com>
2024-06-17 22:15:59 +0800

    新增日历页面

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,11 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../page/Home.jsx";
+import Calendar from "../page/Calendar.jsx";
 
 // 定义路由
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home />,
+  },
+  {
+    path: "/calendar",
+    element: <Calendar />,
   },
 ]);
 export default router;
